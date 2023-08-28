e16cf536890a470db9bb87d9ccf3dd60b5e3156f
신창학 <93763809+leon4652@users.noreply.github.com>
2023-08-28 22:20:00 +0900

    [level 2] Title: 구명보트, Time: 7.72 ms, Memory: 53.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.java
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -1,17 +1,19 @@
-import java.util.Arrays;
-
+import java.util.*;
 class Solution {
-public static int solution(int[] people, int limit) {
-        int boat = 0;
+    public int solution(int[] people, int limit) {
+       int boat = 0;
         int left = 0;
         int right = people.length - 1;
 
         Arrays.sort(people);
 
         while (left <= right) {
+						//가장 가벼운 승객을 탈출시킬 수 있다면 개수 증가
             if (people[left] + people[right] <= limit) {
                 left++;
             }
+						//매 반복문마다 보트 + 1, 가장 무거운 사람은 -1 되게 되어있음.
+						//근데 여기에 가벼운 사람을 태울 수 있다면(위의 if문) 같이 태움
             right--;
             boat++;
         }
