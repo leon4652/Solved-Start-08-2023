194354780c779f7aede49cc65a1bfb4256a009b1
신창학 <93763809+leon4652@users.noreply.github.com>
2023-09-19 08:11:56 +0900

    [level 2] Title: 가장 큰 수, Time: 1.58 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String[] strNumbers = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            strNumbers[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(strNumbers, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return (o2 + o1).compareTo(o1 + o2);
+            }
+        });
+
+        if (strNumbers[0].equals("0")) return "0";
+
+        StringBuilder sb = new StringBuilder();
+        for (String strNumber : strNumbers) {
+            sb.append(strNumber);
+        }
+
+        return sb.toString();
+    }
+}
