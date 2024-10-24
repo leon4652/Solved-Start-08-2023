ed5e566e3a6b0769d5f3e5aa4fdb1decb4428889
leon4652 <93763809+leon4652@users.noreply.github.com>
2024-10-24 23:21:19 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -0,0 +1,11 @@
+-- 코드를 작성해주세요
+
+select id 
+  from ecoli_data 
+ where parent_id in (
+     select id 
+       from ecoli_data 
+      where parent_id in (select id from ecoli_data where parent_id is Null)
+ )
+ order by id
+ 
