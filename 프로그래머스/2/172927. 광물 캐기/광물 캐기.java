936eacf4ec1184ec0b539ce26689c8636180d160
leon4652 <93763809+leon4652@users.noreply.github.com>
2024-10-26 21:31:56 +0900

    [level 2] Title: 광물 캐기, Time: 41.63 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -1,73 +1,48 @@
-
-/*
-
-picks = [dia, iron, stone] 순서, 각각 곡괭이의 개수를 의미, 각 곡괭이는 5개의 광물을 캐면 사용 불가
-minerals = 광물의 순서
-*/
-
+import java.io.*;
 import java.util.*;
 class Solution {
-    static int maxIdx;
-    static int minRes = Integer.MAX_VALUE; //결과
-    static int[][] chart = 
-    {
-        {1, 1, 1}, //다이아 곡괭이 - 다이아, 철, 돌
-        {5, 1, 1}, //철 곡괭이 - 다이아, 철, 돌
-        {25, 5, 1}, //돌 곡괭이 - 다이아, 철, 돌
+    static int[] minArr;
+    static int answer = Integer.MAX_VALUE;
+    static int[][] breaking = {
+        {1, 1, 1}, //dia
+        {5, 1, 1},
+        {25, 5, 1}
     };
+    
     public int solution(int[] picks, String[] minerals) {
-        maxIdx = minerals.length - 1; //가능한 최대 인덱스
+        minArr = new int[minerals.length];
+        for(int i = 0 ; i < minerals.length; i++) {
+            if(minerals[i].startsWith("d")) minArr[i] = 0;
+            else if(minerals[i].startsWith("i")) minArr[i] = 1;
+            else if(minerals[i].startsWith("s")) minArr[i] = 2;
+            else throw new RuntimeException("str -> int fail");
+        }
         
-        solve(picks[0], picks[1], picks[2], minerals, 0, 0, 0); //다이아 곡괭이 사용
-        solve(picks[0], picks[1], picks[2], minerals, 0, 0, 1); //철 곡괭이 사용
-        solve(picks[0], picks[1], picks[2], minerals, 0, 0, 2); //돌 곡괭이 사용
-        return minRes;
+        backTracking(0, 0, 0, 0, picks[0], picks[1], picks[2]);
+        return answer;
     }
     
-    
-    public void solve(int diaPix, int ironPix, int stonePix, String minerals[], int fatigue, int idx, int nowUse) {
+    public void backTracking(int cnt, int idx, int nowUseType, int pixRemain, int diaPix, int ironPix, int stonePix) {
         
-        if(fatigue >= minRes) return; //최대 피로도 넘김
-        if(diaPix == 0 && ironPix == 0 && stonePix == 0) { //모든 곡괭이 사용
-            minRes = Math.min(minRes, fatigue); //정산
-            System.out.println("정산 : " + fatigue);
-            return; 
+        //캘 수 있을 경우
+        while(pixRemain > 0 && idx < minArr.length) {
+            int minType = minArr[idx]; //광물
+            cnt += breaking[nowUseType][minType];
+            pixRemain--; //내구도 감소
+            idx++; //다음 광물
         }
-        if(nowUse == 0 && diaPix == 0 || nowUse == 1 && ironPix == 0 || nowUse == 2 && stonePix == 0) {
-            return; //해당 곡괭이 사용 불가
-        }
-
-
-        //곡괭이 소모
-        if(nowUse == 0) diaPix--;
-        else if(nowUse == 1) ironPix--;
-        else stonePix--;
         
-        int count = 5; //한 곡괭이에 최대 5번 사용
-        while(count-- > 0) {
-            
-            //광물 번호로 치환
-            int mineralNum; 
-            if(minerals[idx].equals("diamond")) mineralNum = 0;
-            else if(minerals[idx].equals("iron")) mineralNum = 1;
-            else mineralNum = 2;
-            
-            //자원 캐기(피로도 증가)
-            fatigue += chart[nowUse][mineralNum];
-            
-            //인덱스 증가 및 전부 캤는지 확인
-            idx++;
-            if(idx > maxIdx) {
-                minRes = Math.min(minRes, fatigue);
-                return;
-            }
-        }
-
-        //다음 재귀 시행
-        solve(diaPix, ironPix, stonePix, minerals, fatigue, idx, 0);
-        solve(diaPix, ironPix, stonePix, minerals, fatigue, idx, 1);
-        solve(diaPix, ironPix, stonePix, minerals, fatigue, idx, 2);
-
         
+        //다음 곡괭이 사용
+        if(diaPix > 0) backTracking(cnt, idx, 0, 5, diaPix - 1, ironPix, stonePix);    
+        if(ironPix > 0) backTracking(cnt, idx, 1, 5, diaPix, ironPix - 1, stonePix);
+        if(stonePix > 0) backTracking(cnt, idx, 2, 5, diaPix, ironPix, stonePix - 1);
+        
+        //전부 캤거나 전부 소모
+        if(idx >= minArr.length || (diaPix == 0 && ironPix == 0 && stonePix == 0 && pixRemain == 0)) { 
+            
+            answer = Math.min(cnt, answer);
+            return;
+        }        
     }
 }
